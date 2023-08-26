580abaaca29809b6fd48368a81bd9620650c9103
amir <ahoseini163@gmail.com>
2023-08-26 19:42:09 +0330

    first

---
+++ app/src/main/java/com/example/googlesign_in/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.googlesign_in
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.IntentSenderRequest
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.googlesign_in.presentation.sign_in.GoogleAuthUiClient
+import com.example.googlesign_in.presentation.sign_in.SignInScreen
+import com.example.googlesign_in.presentation.sign_in.SignInVIewModel
+import com.example.googlesign_in.ui.theme.GoogleSignInTheme
+import com.google.android.gms.auth.api.identity.Identity
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    private val googleAuthClient by lazy {
+        GoogleAuthUiClient(
+            context = applicationContext,
+            oneTapClient = Identity.getSignInClient(applicationContext)
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GoogleSignInTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "sign_in") {
+                        composable("sign_in") {
+                            val viewModel = viewModel<SignInVIewModel>()
+                            val state by viewModel.state.collectAsStateWithLifecycle()
+
+                            val launcher = rememberLauncherForActivityResult(
+                                contract = ActivityResultContracts.StartIntentSenderForResult(),
+                                onResult = { result ->
+                                    if (result.resultCode == RESULT_OK) {
+                                        lifecycleScope.launch {
+                                            val signInResult = googleAuthClient.signInWithIntent(
+                                                intent = result.data ?: return@launch
+                                            )
+                                            viewModel.onSignInResult(signInResult)
+                                        }
+                                    }
+                                }
+                            )
+                            LaunchedEffect(key1 = state.isSignInSuccessful) {
+                                if (state.isSignInSuccessful) {
+                                    Toast.makeText(
+                                        applicationContext,
+                                        "Sign in successful",
+                                        Toast.LENGTH_LONG
+                                    ).show()
+                                }
+                            }
+                            SignInScreen(
+                                state = state,
+                                onSignClick = {
+                                    lifecycleScope.launch {
+                                        val signInIntentSender = googleAuthClient.signIn()
+                                        launcher.launch(
+                                            IntentSenderRequest.Builder(
+                                                signInIntentSender ?: return@launch
+                                            ).build()
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/googlesign_in/presentation/profile/ProfileScreen.kt
@@ -0,0 +1,5 @@
+package com.example.googlesign_in.presentation.profile
+
+import androidx.compose.runtime.Composable
+
+
